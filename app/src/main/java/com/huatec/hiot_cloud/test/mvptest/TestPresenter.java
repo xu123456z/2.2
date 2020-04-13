5eda922500834db3d6175eeb1846b5f4ce677d1c
wangchun <1109046535@qq.com>
2020-04-13 15:35:50 +0800

    添加MVP测试

--- app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestPresenter.java
+++ app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestPresenter.java
@@ -11,7 +11,7 @@ public class TestPresenter {
         this.view = view;
     }
     public  void login(User user){
-        if ("lisi".equals(user.getUserName()) && "123".equals(user.getPassword())){
+        if ("wangchun".equals(user.getUserName()) && "123456".equals(user.getPassword())){
             view.showMessage("登录成功");
         }else{
             view.showMessage("登录失败");
